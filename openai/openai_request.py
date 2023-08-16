ad7bca23c900418ea310a61f3b96a442bbe92fbb
Daniel Bodenstein <danielboden111@gmail.com>
2023-08-16 16:37:35 +0300

    dialogue_with_openai

--- openai/openai_request.py
+++ openai/openai_request.py
@@ -1,5 +1,6 @@
 import openai
 from config import Config
+import asyncio
 
 
 with open(r'C:\Users\User\Desktop\exellenteam\bot\telegram-telegram-bot-group-6\openai\.env', "r") as file:
@@ -11,19 +12,43 @@ def generate_prompt(estimate_problem):
     return Config.TEMPLATE_PREFIX_TO_OPENAI.format(estimate_problem)
 
 
-def ask_openai(prompt):
-    response = openai.ChatCompletion.create(
-        model=Config.MODEL,
-        max_tokens=Config.MAX_TOKENS,
-        messages=[{"role": "user", "content": prompt}]
-    )
-    print(response.choices[0].message.content)
+async def async_ask_openai(messages):
+
+    try:
+        response = await openai.ChatCompletion.acreate(
+            model=Config.MODEL,
+            max_tokens=Config.MAX_TOKENS,
+            messages=messages,
+            timeout=Config.TIMEOUT
+        )
+        content = response.choices[0].message.content
+        print(content)
+        messages.append({"role": "assistant", "content": content})
+        return content
+
+    except openai.error.RateLimitError as e:
+        print(f'api key get to the limit msg:{e}')
+
+    except openai.error.Timeout as e:
+        print(f'openai time out :{e}')
+
+    except openai.error.AuthenticationError as e:
+        print(f'invalid openAI token: {e}')
 
 
-def main():
+async def main():
+
+    messages = [{"role": "system", "content": "You need to verify the correctness of the disease"}]
     prompt = generate_prompt('Apple Rusk')
-    ask_openai(prompt)
+    messages.append({"role": "user", "content": prompt})
+    await async_ask_openai(messages)
+
+    while True:
+        user_input = input(" ")
+        messages.append({"role": "user", "content": user_input})
+        await async_ask_openai(messages)
+        await asyncio.sleep(1)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
