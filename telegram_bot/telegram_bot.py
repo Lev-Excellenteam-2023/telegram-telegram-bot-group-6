779cdaae12956b7e446b0711b31bee32b21a8d39
Asher <ashmenzer@gmail.com>
2023-08-17 11:06:20 +0300

    add the telegram the functionality to chat with gpt

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -3,10 +3,12 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
     CallbackContext
 from firebase.firebase_db import add_user
 from config import Config
+from openai.openai_request import ask_openai
 
 PHONE = 0
 COUNTRY = 1
 CITY = 2
+DIAGNOSE = 3
 
 COMMAND_DESCRIPTION = {
     '/start': 'Start the conversation with the bot',
@@ -16,13 +18,13 @@ COMMAND_DESCRIPTION = {
 }
 
 
-async def start_command(update: Update, context: CallbackContext):
+async def start_command(update: Update):
     await update.message.reply_text(
         "Hi! Thanks for chatting with me!\nI am the Plant diseases detector"
         " Bot. Please type /help to see the available commands.")
 
 
-async def help_command(update: Update, context: CallbackContext):
+async def help_command(update: Update):
     help_text = "Here are the available commands:\n\n"
     for command, description in COMMAND_DESCRIPTION.items():
         help_text += f"{command}: {description}\n"
@@ -30,7 +32,7 @@ async def help_command(update: Update, context: CallbackContext):
     await update.message.reply_text(help_text)
 
 
-async def open_new_chat_command(update: Update, context: CallbackContext):
+async def open_new_chat_command(update: Update):
     await update.message.reply_text(
         "Okay, before we continue, let's start with your phone number please.")
     return PHONE
@@ -54,7 +56,6 @@ async def get_country(update: Update, context: CallbackContext):
 async def get_city(update: Update, context: CallbackContext):
     city = update.message.text
     context.user_data['city'] = city
-
     # Get user information from user_data
     phone = context.user_data['phone']
     country = context.user_data['country']
@@ -63,7 +64,26 @@ async def get_city(update: Update, context: CallbackContext):
     add_user(phone, update.effective_user.full_name, country, city)  # Modify this line according to your function
 
     await context.bot.send_message(update.message.chat_id, "Thank you! Your information has been saved.")
-    return ConversationHandler.END
+    return DIAGNOSE
+
+
+async def validate_diagnose(update: Update, context: CallbackContext, estimated_problem: str):
+    if 'gpt_conv' not in context.user_data:
+        context.user_data['gpt_conv'] = {
+            {'role': 'system', 'content': Config.TEMPLATE_PREFIX_TO_OPENAI.format(estimated_problem)}}
+        reply = ask_openai(context.user_data['gpt_conv'])
+        add_msg_to_gpt_conv(context, role='assistant', content=reply)
+        return DIAGNOSE
+
+    content = update.message.text
+    add_msg_to_gpt_conv(context, role='user', content=content)
+    reply = ask_openai(context.user_data['gpt_conv'])
+    add_msg_to_gpt_conv(context, role='assistant', content=reply)
+    return DIAGNOSE
+
+
+def add_msg_to_gpt_conv(context: CallbackContext, role: str, content: str):
+    context.user_data['gpt_conv'].append({'role': {role}, 'content': {content}})
 
 
 async def get_user_input(update, context):
@@ -79,7 +99,7 @@ async def handle_invalid_input(update: Update, context: CallbackContext):
     await context.bot.send_message(update.message.chat_id, "Invalid input. Please provide the requested information.")
 
 
-async def cancel(update: Update, context: CallbackContext):
+async def cancel(update: Update):
     await update.message.reply_text("Conversation canceled.")
     return ConversationHandler.END
 
@@ -96,7 +116,8 @@ def main():
         states={
             PHONE: [MessageHandler(filters.Text and filters.Regex(r'^(?!/)') and filters.Regex(r'^\d+$'), get_phone)],
             COUNTRY: [MessageHandler(filters.Text and filters.Regex(r'^(?!/)'), get_country)],
-            CITY: [MessageHandler(filters.Text and filters.Regex(r'^(?!/)'), get_city)]
+            CITY: [MessageHandler(filters.Text and filters.Regex(r'^(?!/)'), get_city)],
+            DIAGNOSE: [MessageHandler(filters.Text and filters.Regex(r'^(?!/)'), validate_diagnose)]
         },
         fallbacks=[CommandHandler('cancel', cancel)]
 
