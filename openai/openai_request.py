e235408497803af72e2e79fffe05fbcb910b7d9a
Daniel Bodenstein <danielboden111@gmail.com>
2023-08-17 11:04:02 +0300

    changes_in_ask_openai

--- openai/openai_request.py
+++ openai/openai_request.py
@@ -5,19 +5,20 @@ from config import Config
 import asyncio
 
 
-with open(r'C:\Users\yehuda\Documents\ExcelenTeam\Plant-Telegram-Bot\openai\env', "r") as file:
+with open(r'C:\Users\User\Desktop\exellenteam\bot\telegram-telegram-bot-group-6\openai\.env', "r") as file:
     OPENAI_KEY = file.read().strip()
 openai.api_key = OPENAI_KEY
 
 
 def generate_prompt(estimate_problem):
+    estimate_problem_info = generate_condition_info()
     return Config.TEMPLATE_PREFIX_TO_OPENAI.format(estimate_problem)
+
 
 def generate_condition_info(condition):
     condition_info = disease_dic[condition]
     print(condition_info)
     return condition_info
-
 
 
 async def async_ask_openai(messages):
@@ -28,10 +29,7 @@ async def async_ask_openai(messages):
             messages=messages,
             timeout=Config.TIMEOUT
         )
-        content = response.choices[0].message.content
-        print(content)
-        messages.append({"role": "assistant", "content": content})
-        return content
+        return response.choices[0].message.content
 
     except openai.error.RateLimitError as e:
         print(f'api key get to the limit msg:{e}')
@@ -42,20 +40,3 @@ async def async_ask_openai(messages):
     except openai.error.AuthenticationError as e:
         print(f'invalid openAI token: {e}')
 
-
-async def main():
-
-    messages = [{"role": "system", "content": "You need to verify the correctness of the disease"}]
-    prompt = generate_prompt('Apple Rusk')
-    messages.append({"role": "user", "content": prompt})
-    await async_ask_openai(messages)
-
-    while True:
-        user_input = input(" ")
-        messages.append({"role": "user", "content": user_input})
-        await async_ask_openai(messages)
-        await asyncio.sleep(1)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
